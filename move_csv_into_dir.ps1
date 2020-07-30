2731044a0bf19073dd194b2c2b4626a967e2a299
Tory <57686840+ToryDav@users.noreply.github.com>
2020-07-30 11:30:22 -0400

    Add files via upload

---
+++ move_csv_into_dir.ps1
@@ -0,0 +1,14 @@
+﻿# Please run this script first to match and move CSV files from the scripts such as CDP to CSV or VLAN to CSV
+# Please change the $seedPath variable to match your system path, pleasue use an absolute path, not a relative path, this script must be in the 'ScriptOutput' directory.
+$seedPath = 'C:\Users\torydav\OneDrive - CDW\Tory\My Documents\Projects\Scripting\CDW Scripts\securecrt-tools-master\ScriptOutput'
+
+# Don't Edit Below this line 
+# -------------------------------------------------------------------------------
+$dirs = Get-ChildItem -Path $seedPath -Directory | select -ExpandProperty Name 
+
+foreach ($directory in $dirs)
+{
+    move-item -Path $seedPath\$directory*.csv -Destination $seedPath\$directory\
+}
+
+Write-Host -ForegroundColor Green "DONE!"
